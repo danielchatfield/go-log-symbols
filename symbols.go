c266b7031b8841eab38c4caa2c41064337a70bf5
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-12-11 23:53:07 +0000

    Added logsymbols

---
+++ symbols.go
@@ -0,0 +1,10 @@
+package logsymbols
+
+// The Log symbols for unix OSs
+var (
+	Info    = "ℹ"
+	Success = "✔"
+	Warning = "⚠"
+	Error   = "✖"
+	Spinner = []string{"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"}
+)
